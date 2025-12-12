60eedb11dbc2414f7ee6a9053d663ed1ac9c0b89
Taimoor Ali <taimoorali202@gmail.com>
2025-12-12 12:23:32 +0500

    update migrations

---
+++ prisma/migrations/20251212070321_added_link_model/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "Link" (
+    "id" SERIAL NOT NULL,
+    "shortCode" TEXT NOT NULL,
+    "longUrl" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "usedCount" INTEGER NOT NULL DEFAULT 0,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Link_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Link_shortCode_key" ON "Link"("shortCode");
