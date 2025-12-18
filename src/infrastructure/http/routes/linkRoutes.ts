79f496bc3096eddb8db13f573b9f163fee33b1cc
Taimoor Ali <taimoorali202@gmail.com>
2025-12-18 21:20:19 +0500

    remove unused import

--- src/infrastructure/http/routes/linkRoutes.ts
+++ src/infrastructure/http/routes/linkRoutes.ts
@@ -2,7 +2,6 @@ import express from "express";
 import makeLongUrlToShortUrlController from "../controllers/LongUrlToShortUrlController.js";
 import { authMiddleware } from "../middlewares/AuthMiddleware.js";
 import requireAuth from "../requireAuth.js";
-import requireBody from "../middlewares/ValidateBody.js";
 import validateBody from "../middlewares/ValidateBody.js";
 
 const router = express.Router();
