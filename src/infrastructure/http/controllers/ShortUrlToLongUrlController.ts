4c8dc471c4cb011266247e187adf5510722b1c58
Taimoor Ali <taimoorali202@gmail.com>
2025-12-19 19:28:22 +0500

    define the controller for this endpoint

---
+++ src/infrastructure/http/controllers/ShortUrlToLongUrlController.ts
@@ -0,0 +1,34 @@
+import type { Request, NextFunction, Response } from "express";
+import ShortUrlToLongUrlUseCase from "../../../application/useCases/shortUrlToLongUrl/UseCase.js";
+import prisma from "../../database/client.js";
+import LinkRepository from "../../database/implementations/LinkRepository.js";
+import AppError from "../../../shared/errors/AppError.js";
+import RequestDTO from "../../../application/useCases/shortUrlToLongUrl/RequestDTO.js";
+import InvalidShortUrlError from "../../../domain/entities/link/errors/InvalidShortUrlError.js";
+import ShortCodeDoesNotExistError from "../../../domain/entities/link/errors/ShortCodeDoesNotExistError.js";
+
+function makeShortUrlToLongUrlController() {
+  const linkRepository: LinkRepository = new LinkRepository(prisma);
+  const shortUrlToLongUrlUseCase = new ShortUrlToLongUrlUseCase({ linkRepository });
+
+  async function redirectToLongUrl(req: Request, res: Response, next: NextFunction) {
+    try {
+      const shortCode = req.params.shortCode as string;
+      
+      const dto = new RequestDTO({ shortCode });
+      const result = await shortUrlToLongUrlUseCase.execute(dto);
+
+      res.redirect(result.longUrl);
+    } catch (err) {
+      if (err instanceof InvalidShortUrlError || err instanceof ShortCodeDoesNotExistError) {
+        return res.status(404).send('404 Not Found');
+      }
+
+      return next(err);
+    }
+  }
+
+  return { redirectToLongUrl };
+}
+
+export default makeShortUrlToLongUrlController;
