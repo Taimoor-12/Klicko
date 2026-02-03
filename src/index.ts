ed47d0aa926a33bf143131769719f7a8c3cecc46
Taimoor Ali <taimoorali202@gmail.com>
2026-02-03 13:43:16 +0500

    fix indentation

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ async function main() {
   const app = createApp();
 
   app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`)
+    console.log(`Server running on http://localhost:${port}`)
   });
 }
 
