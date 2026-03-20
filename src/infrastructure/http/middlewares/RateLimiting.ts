cd17eed552dd007957541c9418c37f329f1bcda5
Taimoor Ali <taimoorali202@gmail.com>
2026-03-20 17:49:16 +0500

    apply rate limiting on userId or email

--- src/infrastructure/http/middlewares/RateLimiting.ts
+++ src/infrastructure/http/middlewares/RateLimiting.ts
@@ -3,15 +3,25 @@ import { RedisStore } from "rate-limit-redis";
 import RedisClient from "../../memory-store/client.js";
 import type { AuthenticatedRequest } from "../../http/AuthenticatedRequest";
 
-function makeRateLimiter(windowMs: number, limit: number) {
+type KeyType = 'ip' | 'userId' | 'email';
+
+function makeRateLimiter(windowMs: number, limit: number, keyType: KeyType = 'ip') {
   return rateLimit({
     windowMs: windowMs,
     limit: limit,
-    standardHeaders: true,
+    standardHeaders: true,  
     legacyHeaders: false,
     keyGenerator: (req) => {
-      const authenticatedReq = req as AuthenticatedRequest;
-      return authenticatedReq.user ? authenticatedReq.user.userId : ipKeyGenerator(req.ip ?? '')
+      switch (keyType) {
+        case 'userId':
+          const authenticatedReq = req as AuthenticatedRequest;
+          return authenticatedReq.user?.userId ?? ipKeyGenerator(req.ip ?? '');
+        case 'email':
+          return req.body?.email ?? ipKeyGenerator(req.ip ?? '');
+        case 'ip':
+        default:
+          return ipKeyGenerator(req.ip ?? '');
+      }
     },
 
     store: new RedisStore({
