dee88cef95d0ab4cf1867e06901ef5203bb66834
Taimoor Ali <taimoorali202@gmail.com>
2026-03-25 15:27:56 +0500

    rename variables

--- src/infrastructure/http/routes/authRoutes.ts
+++ src/infrastructure/http/routes/authRoutes.ts
@@ -9,8 +9,15 @@ const router = express.Router();
 const registerUserController =  makeRegisterUserController();
 const loginUserController = makeLoginUserController();
 
-router.post('/register', makeRateLimiter(60 * 60 * 1000, 5), registerUserController.registerUser);
-router.post('/login', loginUserController.loginUser);
+const ONE_HOUR_MS = 60 * 60 * 1000;
+const FIFTEEN_MINUTES_MS = 15 * 60 * 1000;
+
+router.post('/register', makeRateLimiter(ONE_HOUR_MS, 5), registerUserController.registerUser);
+router.post('/login', 
+  makeRateLimiter(FIFTEEN_MINUTES_MS, 10), 
+  makeRateLimiter(FIFTEEN_MINUTES_MS, 10, 'email'), 
+  loginUserController.loginUser
+);
 router.get('/check', authMiddleware, (req: Request, res: Response) => {
   res.status(200).json({
     loggedIn: true,
