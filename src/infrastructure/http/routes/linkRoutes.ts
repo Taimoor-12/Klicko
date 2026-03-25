29fe0f579e1a9f44e773befb4333e4319e132ddd
Taimoor Ali <taimoorali202@gmail.com>
2026-03-25 19:59:32 +0500

    add rate limiting to long to short url endpoint

--- src/infrastructure/http/routes/linkRoutes.ts
+++ src/infrastructure/http/routes/linkRoutes.ts
@@ -3,11 +3,20 @@ import makeLongUrlToShortUrlController from "../controllers/LongUrlToShortUrlCon
 import { authMiddleware } from "../middlewares/AuthMiddleware.js";
 import requireAuth from "../requireAuth.js";
 import validateBody from "../middlewares/ValidateBody.js";
+import makeRateLimiter from "../middlewares/RateLimiting.js";
 
 const router = express.Router();
 
+const ONE_HOUR_MS = 60 * 60 * 1000;
+
 const longUrlToShortUrlController = makeLongUrlToShortUrlController();
 
-router.post('/shorten', authMiddleware, validateBody(["longUrl"]), requireAuth(longUrlToShortUrlController.convertLongUrlToShort));
+router.post(
+  "/shorten",
+  authMiddleware,
+  validateBody(["longUrl"]),
+  makeRateLimiter({ windowMs: ONE_HOUR_MS, limit: 50, keyType: 'userId', prefix: 'rl:long_url:'}),
+  requireAuth(longUrlToShortUrlController.convertLongUrlToShort),
+);
 
 export default router;
