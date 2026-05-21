c180f5c24cd013ea3fd5247e3d497e741d723647
Taimoor Ali <taimoorali202@gmail.com>
2026-05-21 15:29:22 +0500

    define longToShort api function

---
+++ frontend/lib/api/link.ts
@@ -0,0 +1,13 @@
+import request from "./client";
+
+export type LongToShortResponse = {
+  shortUrl: string;
+};
+
+export const linkApi = {
+  longToShortUrl: (data: { longUrl: string }) =>
+    request<LongToShortResponse>('/api/urls/shorten', {
+      method: 'POST',
+      body: data
+    }),
+}
