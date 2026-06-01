767278bbb27842670b2ef767cee1b44fa4c278e1
Taimoor Ali <taimoorali202@gmail.com>
2026-06-01 20:50:30 +0500

    add pagination component

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -5,6 +5,7 @@ import { userApi } from "@/lib/api";
 import { cookies } from "next/headers";
 import { LongUrlInputWrapper } from "@/components/dashboard/long-url-input-wrapper";
 import { LinksTable } from "@/components/dashboard/links-table";
+import { PaginationDashboard } from "@/components/dashboard/pagination";
 
 export default async function Page() {
   const cookieStore = await cookies();
@@ -63,6 +64,9 @@ export default async function Page() {
         <div className="mt-16">
             <h3 className="text-lg md:text-2xl font-bold mb-6">Your Links</h3>
             <LinksTable links={linksRes.data}/>
+        </div>
+        <div className="mt-8">
+            <PaginationDashboard />
         </div>
       </main>
     </>
